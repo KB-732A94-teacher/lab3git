20bf857cff3f69afae7bce46eaa6d3077cda8077
Josh <gyommy15@gmail.com>
2017-09-18 21:06:41 +0200

    update "wiki_graph"

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' Example data set for Dijkstra algorithm in the package
+#'
+#' A dataset containing the information of origin node, destination node and weight
+#'
+#' @format A data frame with 3 columns with 18 variables:
+#' \describe{
+#'   \item{v1}{index of starting node}
+#'   \item{v2}{index of destination node}
+#'   \item{w}{weight of the edge from column1 to column2 component}
+#'   ...
+#' }
+#' @source \url{https://en.wikipedia.org/wiki/Dijkstra's_algorithm}
+"wiki_graph"
